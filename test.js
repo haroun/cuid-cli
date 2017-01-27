c6abf6d8a9464d4ebeeb59b8bcba2e494ced7a48
Harouna Traoré <h.traore@icloud.com>
2017-01-27 17:48:57 +0100

    add tests

---
+++ test.js
@@ -0,0 +1,35 @@
+import test from 'tape';
+import execa from 'execa';
+
+test('cuid output length', async assert => {
+  const {stdout} = await execa('./cli.js', [4]);
+
+  const actual = stdout.split('\n').length;
+  const expected = 4;
+
+  assert.equal(actual, expected, 'cuid output should contains 4 lines if 4 is provided');
+
+  assert.end();
+});
+
+test('cuid output content', async assert => {
+  const {stdout} = await execa('./cli.js', [4]);
+
+  const actual = Array.from(new Set(stdout.split('\n'))).length;
+  const expected = 4;
+
+  assert.equal(actual, expected, 'cuid should contain unique values only');
+
+  assert.end();
+});
+
+test('cuid slug length', async assert => {
+  const {stdout} = await execa('./cli.js', ['--slug']);
+
+  const actual = stdout.length;
+  const expected = 10;
+
+  assert.equal(actual, expected, 'slug should be 10 characters long');
+
+  assert.end();
+});
