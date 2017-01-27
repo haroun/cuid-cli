199ce3302875080bc959e79e3aa299bddf94c5ad
Harouna Traoré <h.traore@icloud.com>
2017-01-27 18:13:56 +0100

    test using stdin

--- test.js
+++ test.js
@@ -33,3 +33,14 @@ test('cuid slug length', async assert => {
 
   assert.end();
 });
+
+test('using stdin', async assert => {
+	const {stdout} = await execa('./cli.js', {
+		input: 0
+	});
+
+  const actual = stdout.length;
+  const expected = 1;
+
+  assert.equal(actual, expected, 'cuid should work with stdin and stdout should contain 1 entry');
+});
