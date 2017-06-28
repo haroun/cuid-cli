a27444950aa225fe235947c9bcbdf3adf287b0c9
Harouna Traore <h.traore@icloud.com>
2017-06-28 21:14:19 +0200

    add message var

--- test.js
+++ test.js
@@ -2,12 +2,19 @@ import test from 'tape';
 import execa from 'execa';
 
 test('cuid output length', async assert => {
-  const {stdout} = await execa('./cli.js', [4]);
+  const {stdout} = await execa('./cli.js');
+  assert.true(stdout.length > 0);
+});
+
+test('cuid output length', async assert => {
+  const {stdout} = await execa('./cli.js', ['4']);
+
+  const message = 'cuid output should contains 4 lines if 4 is provided';
 
   const actual = stdout.split('\n').length;
   const expected = 4;
 
-  assert.equal(actual, expected, 'cuid output should contains 4 lines if 4 is provided');
+  assert.equal(actual, expected, message);
 
   assert.end();
 });
@@ -15,10 +22,12 @@ test('cuid output length', async assert => {
 test('cuid output content', async assert => {
   const {stdout} = await execa('./cli.js', [4]);
 
+  const message = 'cuid should contain unique values only';
+
   const actual = Array.from(new Set(stdout.split('\n'))).length;
   const expected = 4;
 
-  assert.equal(actual, expected, 'cuid should contain unique values only');
+  assert.equal(actual, expected, message);
 
   assert.end();
 });
@@ -26,10 +35,12 @@ test('cuid output content', async assert => {
 test('cuid slug length', async assert => {
   const {stdout} = await execa('./cli.js', ['--slug']);
 
+  const message = 'slug should be 10 characters long';
+
   const actual = stdout.length;
   const expected = 10;
 
-  assert.equal(actual, expected, 'slug should be 10 characters long');
+  assert.equal(actual, expected, message);
 
   assert.end();
 });
@@ -39,10 +50,12 @@ test('using stdin', async assert => {
     input: 0
   });
 
+  const message = 'cuid should work with stdin and stdout should contain 1 entry';
+
   const actual = stdout.length;
   const expected = 1;
 
-  assert.equal(actual, expected, 'cuid should work with stdin and stdout should contain 1 entry');
+  assert.equal(actual, expected, message);
 
   assert.end();
 });
