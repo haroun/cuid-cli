b8867203b66b6da56b3875d2677f05a50f6e08de
Harouna Traore <h.traore@icloud.com>
2017-11-10 01:11:42 +0100

    test stdin

--- test.js
+++ test.js
@@ -2,19 +2,14 @@ import test from 'tape';
 import execa from 'execa';
 
 test('cuid output length', async assert => {
-  const {stdout} = await execa('./cli.js');
-  assert.true(stdout.length > 0);
-});
+  const {stdout} = await execa('./cli.js', [1]);
 
-test('cuid output length', async assert => {
-  const {stdout} = await execa('./cli.js', ['4']);
+  const message = 'slug should be more than 10 characters long';
 
-  const message = 'cuid output should contains 4 lines if 4 is provided';
+  const actual = stdout.length;
+  const expected = 10;
 
-  const actual = stdout.split('\n').length;
-  const expected = 4;
-
-  assert.equal(actual, expected, message);
+  assert.true(actual > expected, message);
 
   assert.end();
 });
@@ -33,29 +28,40 @@ test('cuid output content', async assert => {
 });
 
 test('cuid slug length', async assert => {
-  const {stdout} = await execa('./cli.js', ['--slug']);
+  const {stdout} = await execa('./cli.js', [1, '--slug']);
 
-  const message = 'slug should be 10 characters long';
+  const message = 'slug should be up to 10 characters long';
 
   const actual = stdout.length;
   const expected = 10;
+
+  assert.true(actual <= expected, message);
+
+  assert.end();
+});
+
+test('using stdin', async assert => {
+  const {stdout} = await execa('./cli.js', [], {input: '1'});
+
+  const message = 'cuid should work with stdin and stdout should contain 1 entry';
+
+  const actual = Array.from(new Set(stdout.split('\n'))).length;
+  const expected = 1;
 
   assert.equal(actual, expected, message);
 
   assert.end();
 });
 
-test('using stdin', async assert => {
-  const {stdout} = await execa('./cli.js', {
-    input: 0
-  });
+test('using stdin slug', async assert => {
+  const {stdout} = await execa('./cli.js', ['--slug'], {input: '1'});
 
-  const message = 'cuid should work with stdin and stdout should contain 1 entry';
+  const message = 'cuid should work with stdin and stdout should contain a slug entry';
 
   const actual = stdout.length;
-  const expected = 1;
+  const expected = 10;
 
-  assert.equal(actual, expected, message);
+  assert.true(actual <= expected, message);
 
   assert.end();
 });
